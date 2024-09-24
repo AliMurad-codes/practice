ee4836e43d3ca71be55c9c0bfb83146fcce69300
AliMurad-codes <ranaali0601@gmail.com>
2024-09-24 19:12:24 +0500

    commit

--- pythonp.py
+++ pythonp.py
@@ -1,3 +1,4 @@
+# is paindrome:
 def ispalindrome(str):
     startindex = 0
     endindex = len(str) -1
@@ -9,4 +10,22 @@ def ispalindrome(str):
 
 print(ispalindrome('rececar'))
 
+
+# fibnocci series
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(10))
+
+# find max numer
+
+a = [0 ,1,2,3,4,5,6,7,8,9]
+max_number = a[0]
+for i in range(len(a)):
+    if i > max_number:
+        max_number = i
+print(max_number)
 
